068fbb6a05f8520f1e5c15a1228c8e4c8401ac6b
2seungho2 <73834492+2seungho2@users.noreply.github.com>
2021-09-30 08:03:32 +0900

    Complete

---
+++ Assets/04.Scripts/GameManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum GameState
+{
+    Ready,
+    Play,
+    End
+}
+
+public class GameManager : MonoBehaviour
+{
+    public GameState GS;
+
+    public Hole[] holes;
+    public float limitTime;
+    public Text timeText;
+    public int count_Bad;
+    public int count_Good;
+
+    public GameObject finishGUI;
+    public Text final_Count_Bad;
+    public Text final_Count_Good;
+    public Text final_Score;
+
+    public AudioClip ready_Audio;
+    public AudioClip go_Audio;
+    public AudioClip end_Audio;
+    
+    private void Start()
+    {
+        GetComponent<AudioSource>().clip = ready_Audio;
+        GetComponent<AudioSource>().Play();
+        limitTime = 10.0f;
+    }
+
+    public void Go()
+    {
+        GS = GameState.Play;
+        GetComponent<AudioSource>().clip = go_Audio;
+        GetComponent<AudioSource>().Play();
+    }
+
+    void Update()
+    {
+        if (GS == GameState.Play)
+            limitTime -= Time.deltaTime;
+        if (limitTime <= 0)
+        {
+            limitTime = 0;
+            End();
+        }
+        timeText.text = string.Format("{0:N2}", limitTime);
+    }
+
+    void End()
+    {
+        GS = GameState.End;
+        final_Count_Bad.text = string.Format("나쁜 두더지:" + "{0}", count_Bad);
+        final_Count_Good.text = string.Format("착한 두더지:" + "{0}", count_Good);
+        final_Score.text = string.Format("총점: " + "{0}", count_Bad * 100 - count_Good * 1000);
+        finishGUI.gameObject.SetActive(true);
+    }   
+}
---
+++ Assets/04.Scripts/Hole.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum HoleState
+{
+    None,
+    Open,
+    Idle,
+    Close,
+    Catch
+}
+
+
+public class Hole : MonoBehaviour
+{
+    
+    public HoleState HS;
+
+    public Texture[] open_Images;
+    public Texture[] idle_Images;
+    public Texture[] close_Images;
+    public Texture[] catch_Images;
+
+    public bool goodMole;
+    public int perGood = 20;
+
+    public Texture[] open2_Images;
+    public Texture[] idle2_Images;
+    public Texture[] close2_Images;
+    public Texture[] catch2_Images;
+
+    public float ani_Speed;
+    public float ani_Time;
+
+    int ani_Count;
+
+    public AudioClip open_Audio;
+    public AudioClip catch_Audio;
+
+    public GameManager GM;
+        
+    void Update()
+    {
+        if (ani_Time >= ani_Speed)
+        {
+            if (HS == HoleState.Open)
+            {
+                Open_Ing();
+            }
+            if (HS == HoleState.Idle)
+            {
+                Idle_Ing();
+            }
+            if (HS == HoleState.Close)
+            {
+                Close_Ing();
+            }
+            if (HS == HoleState.Catch)
+            {
+                Catch_Ing();
+            }
+            ani_Time = 0;
+        }
+        else ani_Time += Time.deltaTime;
+
+        if (GM.limitTime <= 0)
+        { 
+            this.gameObject.SetActive(false);
+            HS = HoleState.None;
+            GetComponent<AudioSource>().Stop();
+        }
+    }
+
+    public void Open_On()
+    {
+        HS = HoleState.Open;
+        ani_Count = 0;
+        GetComponent<AudioSource>().clip = open_Audio;
+        GetComponent<AudioSource>().Play();
+        int a = Random.Range(0, 100);
+        if (a <= perGood)
+            goodMole = true;
+        else goodMole = false;
+        if (GM.GS == GameState.Ready)
+            GM.Go();
+    }
+
+    public void Open_Ing()
+    {
+        if(goodMole == false) 
+            GetComponent<Renderer>().material.mainTexture = open_Images[ani_Count];
+        else GetComponent<Renderer>().material.mainTexture = open2_Images[ani_Count];
+        ani_Count += 1;
+        if (ani_Count >= open_Images.Length)
+        {
+            Idle_On();
+        }
+    }
+
+    public void Idle_On()
+    {
+        HS = HoleState.Idle;
+        ani_Count = 0;
+    }
+
+    public void Idle_Ing()
+    {
+        if(goodMole == false)
+            GetComponent<Renderer>().material.mainTexture = idle_Images[ani_Count];
+        else GetComponent<Renderer>().material.mainTexture = idle2_Images[ani_Count];
+        ani_Count += 1;
+        if (ani_Count >= idle_Images.Length)
+        {
+            Close_On();
+        }
+    }
+
+    public void Close_On()
+    {
+        HS = HoleState.Close;
+        ani_Count = 0;
+    }
+
+    public void Close_Ing()
+    {
+        if(goodMole == false)
+            GetComponent<Renderer>().material.mainTexture = close_Images[ani_Count];
+        else GetComponent<Renderer>().material.mainTexture = close2_Images[ani_Count];
+        ani_Count += 1;
+        if (ani_Count >= close_Images.Length)
+        {
+            StartCoroutine(Wait());
+        }
+    }
+
+    public void Catch_On()
+    {
+        HS = HoleState.Catch;
+        ani_Count = 0;
+        GetComponent<AudioSource>().clip = catch_Audio;
+        GetComponent<AudioSource>().Play();
+        if (goodMole == false)
+            GM.count_Bad += 1;
+        else GM.count_Good += 1;
+    }
+
+    public void Catch_Ing()
+    {
+        if(goodMole == false)
+            GetComponent<Renderer>().material.mainTexture = catch_Images[ani_Count];
+        else GetComponent<Renderer>().material.mainTexture = catch2_Images[ani_Count];
+        ani_Count += 1;
+        if (ani_Count >= catch_Images.Length)
+        {
+            StartCoroutine(Wait());
+        }
+    }
+
+    public IEnumerator Wait()
+    {
+        HS = HoleState.None;
+        ani_Count = 0;
+        float wait_Time = Random.Range(0.5f, 4.5f);
+        yield return new WaitForSeconds(wait_Time);
+        Open_On();
+    }
+
+    void OnMouseDown()
+    {
+        if( HS == HoleState.Idle || HS == HoleState.Close)
+            Catch_On();
+    }
+}
